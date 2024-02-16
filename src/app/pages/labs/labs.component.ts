211574c75a684773311ffe10a6a6cae374243218
Karen Alfaro <ksalfarom@gmail.com>
2024-02-16 20:04:16 +0100

    2da clase

---
+++ src/app/pages/labs/labs.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+
+@Component({
+  selector: 'app-labs',
+  standalone: true,
+  imports: [RouterOutlet, CommonModule],
+  templateUrl: './labs.component.html',
+  styleUrl: './labs.component.css'
+})
+export class LabsComponent {
+  wellcome = 'Hola todoapp';
+  tasks = [
+    'Instalar el Angular CLI',
+    'Crear proyecto',
+    'Crear componente'
+  ];
+}
