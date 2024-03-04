2df9f79808e9b7787129b32a89db9f4047a072e0
Karen Alfaro <ksalfarom@gmail.com>
2024-03-04 12:46:21 +0100

    Clases 11 y 12

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from'@angular/common'; 
 
+import{Task} from './../../models/task.model';
+
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -10,20 +12,49 @@ import { CommonModule } from'@angular/common';
 })
 export class HomeComponent {
   wellcome = 'Hola todoapp';
-  tasks = signal ([
-    'Instalar el Angular CLI',
-    'Crear proyecto',
-    'Crear componente',
-    'Crear servicio'
+  tasks = signal<Task[]> ([
+    {
+      id: Date.now(),
+      title: 'Crear proyecto',
+      completed: false
+    },
+    {
+      id: Date.now(),
+      title: 'Crear componentes',
+      completed: false
+    },
   ]);
 
   changeHandler(event: Event){
     const input = event.target as HTMLInputElement;
     const newTask = input.value;
+    this.addTask(newTask);
+  }
+ 
+  addTask(title:string){
+    const newTask = {
+      id: Date.now(),
+      title,
+      completed: false,
+    };
     this.tasks.update((tasks) => [...tasks, newTask]); 
   }
 
   deleteTask(index: number){
     this.tasks.update((tasks) => tasks.filter((task, position) => position !== index));
+  }
+
+  updateTask(index: number){
+    this.tasks.update((tasks) => {
+      return tasks.map((task, position) => {
+        if(position === index){
+          return {
+            ...task,
+            completed: !task.completed
+          }
+        }
+        return task;
+      })
+    })
   }
 }
